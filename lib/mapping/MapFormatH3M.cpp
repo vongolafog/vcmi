cb784764395d549e787dccd5885ef0fca7675c6c
vongolafog <108338817+vongolafog@users.noreply.github.com>
2026-09-19 08:05:04 +0800

    Support HotA hero levels above experience limit

--- lib/mapping/MapFormatH3M.cpp
+++ lib/mapping/MapFormatH3M.cpp
@@ -937,7 +937,12 @@ void CMapLoaderH3M::readPredefinedHeroes()
 			if (cannotGainXP)
 				logGlobal->warn("Map '%s': Option to prevent hero %d from receiveing experience is not implemented!", mapName, heroID);
 
-			if (level > 1)
+			if (level > LIBRARY->heroh->maxSupportedLevel())
+			{
+				if(auto * hero = map->tryGetFromHeroPool(HeroTypeID(heroID)))
+					hero->level = static_cast<ui32>(level);
+			}
+			else if (level > 1)
 				logGlobal->warn("Map '%s': Option to set level of hero %d to %d is not implemented!", mapName, heroID, level);
 		}
 	}
@@ -2512,7 +2517,9 @@ std::shared_ptr<CGObjectInstance> CMapLoaderH3M::readHero(const int3 & mapPositi
 		if (cannotGainXP)
 			logGlobal->warn("Map '%s': Option to prevent hero %d from receiveing experience is not implemented!", mapName, object->subID.num);
 
-		if (level > 1)
+		if (level > LIBRARY->heroh->maxSupportedLevel())
+			object->level = static_cast<ui32>(level);
+		else if (level > 1)
 			logGlobal->warn("Map '%s': Option to set level of hero %d to %d is not implemented!", mapName, object->subID.num, level);
 	}
 	return object;
